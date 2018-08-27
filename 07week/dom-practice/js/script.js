a48dbaae1720d5f89353cd8ea989c290c1355ca2
Darwin <risser.darwin@gmail.com>
2018-08-27 18:32:04 -0500

    sort of working version

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,80 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+window.onload = function(){
+  itemAlert();
+  insertItemCounter();
+  insertAddItem();
+  insertRemoveItemExistingItems();
+}
+
+function listCount(){
+  let list = document.getElementsByTagName("ul");
+  return list[0].children.length;
+}
+
+function itemAlert(){
+  let list = document.getElementsByTagName("ul");
+  alert("there are " + listCount() + " items in your cart");
+}
+
+function insertItemCounter(){
+  let newh2 = document.createElement("h2");
+  let list = document.getElementsByTagName("ul");
+  let listCount = list[0].children.length;
+  newh2.innerHTML = "You have " + listCount + " items in your cart";
+  let countMessage = document.getElementsByTagName("h1");
+  countMessage[0].after(newh2);
+  newh2.setAttribute("id", "item-count");
+}
+
+function insertAddItem(){
+  var newInput = document.createElement("input");
+  newInput.setAttribute("placeholder", "type to add");
+  let cart = document.getElementsByTagName("ul");
+  cart[0].after(newInput);
+  newInput.setAttribute("id", "input-text");
+
+  let newButton = document.createElement("button");
+  newButton.innerHTML = "Add Items";
+  newButton.setAttribute = ("id", "add-button");
+  let nextButton = document.getElementsByTagName("input");
+  nextButton[0].after(newButton);
+
+  newButton.onclick = addItem;
+}
+
+function addItem(event){
+  let newItemText = document.getElementById("input-text");
+  let newItem = document.createElement("li");
+  newItem.innerHTML = newItemText.value;
+  document.getElementsByTagName("ul")[0].appendChild(newItem);
+  document.getElementById("input-text").value="";
+  insertRemoveItem(newItem);
+  updateItemCounter();
+}
+
+function updateItemCounter(){
+  let h2 = document.getElementById("item-count")
+  h2.innerHTML = "You have " +
+  document.getElementsByTagName("ul")[0].children.length + " items in your cart";
+}
+
+function insertRemoveItem (items) {
+  let removeButton = document.createElement("button");
+  removeButton.innerHTML = "Remove";
+  items.appendChild(removeButton);
+  removeButton.onclick = eraseItem;
+}
+
+function eraseItem(){
+  this.parentNode.remove();
+  updateItemCounter();
+}
+
+function insertRemoveItemExistingItems () {
+  let list = document.getElementsByTagName("li");
+  let removeButton = document.createElement("button");
+  removeButton.innerHTML = "Remove";
+  list[0].appendChild(removeButton);
+  removeButton.onclick = eraseItem;
+}
