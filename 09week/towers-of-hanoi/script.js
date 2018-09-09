e250f3d1ca8b706e375e25db04c0fac6a351dcaf
Darwin <risser.darwin@gmail.com>
2018-09-09 11:56:10 -0500

    newer version

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,57 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
+// $(document).ready(function() {
+
+// });
+
+$(function(){
+  $('[data-block]').draggable({
+    revert: 'invalid'
+  });
+
+
+$('[data-stack]').droppable({
+    drop: function(event, ui){
+      let $last = ($(this).children().last()).data('block');
+      let $dragging = $(ui.draggable).data('block');
+      if $dragging > $last{
+        $(ui.draggable).draggable('option', 'revert', true);
+      } else {
+      $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+    }
+    checkWin();
+  }
 });
+])
+function checkWin(){
+  if($("[data-stack=3], [data-stack=2]").children().length === 4)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// $(function(){ if $(['data-block==25']){
+//   $('.draggable').draggable({
+//     revert: 'invalid'
+//   } else
+//
+//
+// )};
+//
+//
+//   $('.drop-area').droppable({
+//     drop: function(event, ui){
+//       $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+//     }
+//   });
+// })
